b5022f3a589869a092c9b1f2c670a81c816cb708
JuniorEscoto <juniorjescoto@gmail.com>
2019-11-10 04:41:34 -0600

    feat: creacion de rutas en el menu superior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,20 @@ import { ServicesModule } from './services/services.module';
 import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { AgmDirectionModule } from 'agm-direction';
 import { environment } from './environments/environment';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'
 import {CrearRutasComponent} from './components/crear-rutas/crear-rutas.component';
-import { HttpClientModule } from '@angular/common/http';
 import {ListaRutasComponent} from './components/lista-rutas/lista-rutas.component';
 import {ListaPasajerosComponent} from './components/lista-pasajeros/lista-pasajeros.component';
+
 // import { AppRoutingModule } from './app.routing.module';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/crear', pathMatch: 'full'},
+  {path: 'crear', component: CrearRutasComponent},
+  {path: 'lista', component: ListaRutasComponent},
+  {path: 'boleto', component: ListaPasajerosComponent},
+]
 
 @NgModule({
   imports:
@@ -30,7 +39,9 @@ import {ListaPasajerosComponent} from './components/lista-pasajeros/lista-pasaje
       }),
       // Componente para las direcciones
       AgmDirectionModule,
-      HttpClientModule
+      HttpClientModule,
+      HttpClientJsonpModule,
+      RouterModule.forRoot(routes, { useHash: true }),
     ],
   declarations: [AppComponent, CrearRutasComponent, ListaRutasComponent, ListaPasajerosComponent],
   providers: [
